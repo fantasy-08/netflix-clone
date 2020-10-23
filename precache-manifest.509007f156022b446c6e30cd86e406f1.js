40f1d3fd0c8451d39d350ff1f400b053552bd0f4
Eshaan <eshaan.263@gmail.com>
2020-10-24 01:19:17 +0530

    Updates

---
+++ precache-manifest.509007f156022b446c6e30cd86e406f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8810de20f9f2dc2d5cb12c88d1bb2222",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "6417d2f0c9473eb2f293",
+    "url": "/netflix-clone/static/css/main.335a190b.chunk.css"
+  },
+  {
+    "revision": "0a8139c955e92c18c376",
+    "url": "/netflix-clone/static/js/2.4bcf0b66.chunk.js"
+  },
+  {
+    "revision": "5268dce82a7d7b285841c98f617d5abc",
+    "url": "/netflix-clone/static/js/2.4bcf0b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6417d2f0c9473eb2f293",
+    "url": "/netflix-clone/static/js/main.efdab123.chunk.js"
+  },
+  {
+    "revision": "87de87812bcb26585514",
+    "url": "/netflix-clone/static/js/runtime-main.19b04377.js"
+  }
+]);
